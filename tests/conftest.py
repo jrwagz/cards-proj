216bd79db9f9a958cb2c1ed40a37bb249df1841f
Justin Wagner <jrwagz@gmail.com>
2022-12-27 20:32:59 -0800

    refactor tests versus exercises. Import initial tests from ch7

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,69 +1,34 @@
 import cards
 import pytest
+from cards import Card
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--func-db",
-        action="store_true",
-        default=False,
-        help="new db for each test",
-    )
+@pytest.fixture(scope="session")
+def tmp_db_path(tmp_path_factory):
+    """Path to temporary database"""
+    return tmp_path_factory.mktemp("cards_db")
 
 
-def db_scope(fixture_name, config):
-    if config.getoption("--func-db", None):
-        return "function"
-    return "session"
-
-
-
-@pytest.fixture(scope=db_scope)
-def db(tmp_path_factory):
-    """CardsDB object connected to a temporary database"""
-    db_path = tmp_path_factory.mktemp("cards_db")
-    db_ = cards.CardsDB(db_path)
+@pytest.fixture(scope="session")
+def session_cards_db(tmp_db_path):
+    """CardsDB"""
+    db_ = cards.CardsDB(tmp_db_path)
     yield db_
     db_.close()
 
 
-
 @pytest.fixture(scope="function")
-def cards_db(db,request,faker):
-    """CardsDB object that's empty"""
+def cards_db(session_cards_db):
+    """Empty CardsDB"""
+    db = session_cards_db
     db.delete_all()
-
-    # Support for `@pytest.mark.num_cards(<some number>)`
-    m = request.node.get_closest_marker("num_cards")
-    if m and len(m.args) > 0:
-        # random seed
-        faker.seed_instance(101)
-        num_cards = m.args[0]
-        for _ in range(num_cards):
-            db.add_card(
-                cards.Card(summary=faker.sentence(),owner=faker.first_name())
-            )
-
     return db
 
 
-@pytest.fixture(scope="session")
-def some_cards():
-    """List of different Card objects"""
-    return [
-        cards.Card("write book", "Brian", "done"),
-        cards.Card("edit book", "Katie", "done"),
-        cards.Card("write 2nd edition", "Brian", "todo"),
-        cards.Card("edit 2nd edition", "Katie", "todo"),
-    ]
-
-
-
 @pytest.fixture(scope="function")
-def non_empty_db(cards_db, some_cards):
-    """CardsDB object that's been populated with 'some_cards'"""
-    for c in some_cards:
-        cards_db.add_card(c)
+def cards_db_three_cards(cards_db):
+    """CardsDB with 3 cards"""
+    cards_db.add_card(Card("foo"))
+    cards_db.add_card(Card("bar"))
+    cards_db.add_card(Card("baz"))
     return cards_db
-
-
