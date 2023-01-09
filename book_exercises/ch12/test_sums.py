b4d3d59a9e7baad279b01dc2a2f3c27d7a6a9698
Justin Wagner <jrwagz@gmail.com>
2023-01-08 21:04:32 -0700

    ch12 exercise 3 done

--- book_exercises/ch12/test_sums.py
+++ book_exercises/ch12/test_sums.py
@@ -1,6 +1,12 @@
 from subprocess import run
+import sums
 
 
 def test_sums_subprocess():
     result = run(["python3", "sums.py", "data.txt"], capture_output=True, text=True)
     assert result.stdout == "200.00\n"
+
+
+def test_sums_import(capsys):
+    sums.main()
+    assert capsys.readouterr().out == "200.00\n"
