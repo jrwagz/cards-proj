e30d444e252068a00c261a3382d74f02196d2150
Justin Wagner <jrwagz@gmail.com>
2023-01-08 21:02:00 -0700

    ch12 exercise 2 done

--- book_exercises/ch12/sums.py
+++ book_exercises/ch12/sums.py
@@ -1,11 +1,17 @@
 # sums.py
 # add the numbers in `data.txt`
 
-sum = 0.0
 
-with open("data.txt", "r") as file:
-    for line in file:
-        number = float(line)
-        sum += number
+def main():
+    sum = 0.0
 
-print(f"{sum:.2f}")
+    with open("data.txt", "r") as file:
+        for line in file:
+            number = float(line)
+            sum += number
+
+    print(f"{sum:.2f}")
+
+
+if __name__ == "__main__":
+    main()
